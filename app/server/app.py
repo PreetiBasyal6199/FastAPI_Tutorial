99fae901237778f0f5d0ab8f4317a5069b6e167e
preeti <preetibasyal412@gmail.com>
2023-05-05 00:00:57 +0545

    added new router

--- app/server/app.py
+++ app/server/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-
+from server.routes.student import router as StudentRouter
 app = FastAPI()
+app.include_router(StudentRouter, tags=["Students"], prefix="/student")
 
 
 @app.get("/", tags=["Root"])
